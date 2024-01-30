e7de2979912be425cd5d9864b440a89175cc28ae
@BrandonWedu <brandon.education@proton.me>
2024-01-30 12:10:35 -0500

    Added functions for click me button

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,7 +10,9 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
-    lateinit var displayTextView: TextView
+    private lateinit var displayTextView: TextView
+    private lateinit var clickMeButton: Button
+    private lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +20,13 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-
+        clickMeButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
         
         // Respond to button click event per specifications
-
+        clickMeButton.setOnClickListener(View.OnClickListener {
+            displayTextView.text = "Hello ${nameEditText.text}"
+        })
 
     }
 }
